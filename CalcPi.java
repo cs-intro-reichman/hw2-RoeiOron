6703337e0d0b4f002aa5981aae66673976c567d6
Roei Oron <roeioron@gmail.com>
2025-11-12 19:33:28 +0200

    new

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,19 @@
-// Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+    public static void main(String [] args) { 
+        int numberOfTimes = Integer.parseInt(args[0]);
+        double j = 1; 
+        double piAdd = 0;
+        double sign = 1; 
+        double pi = Math.PI;
+        for (int i=0; i < numberOfTimes; i++) {
+            double term = sign / j;
+            piAdd = piAdd + term;
+            j = j + 2;
+            sign = sign * -1.0; 
+        }
+        double myPi = piAdd * 4;
+
+        System.out.println("pi, approximated: "+ myPi);
+		System.out.println("pi according to Java: " + pi);
+    }
 }
--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,25 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+        String word = args[0];
+        int numberOfTimes = Integer.parseInt(args[1]);
+        word = word.toUpperCase();
+        String specialLetters = "AEFHILMNORSX";
+        int i = 0;
+        while(i < word.length()) {
+            char letter = word.charAt(i);
+            if (specialLetters.indexOf(letter) != -1) {
+                System.out.println("Give me an " + letter + ": " + letter + "!");
+            } else {
+                System.out.println("Give me a " + letter + ": " + letter + "!");
+            }
+            i++;
         }
+
+        System.out.println("What does that spell?");
+        int j = 0;
+        while (j < numberOfTimes) {
+            System.out.println(word + "!!!");
+            j++;
+        }
+    }
 }
